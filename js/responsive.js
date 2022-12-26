b68f3690a08778cfbb3c9c2e701fbc75552398e9
Erick Yuta <erick_miyagi34231@hotmail.com>
2022-12-26 17:09:44 -0300

    Container Images

--- js/responsive.js
+++ js/responsive.js
@@ -8,28 +8,21 @@ class aside {
     bringNavBar = () => {
         this.navbar.classList.toggle(this.active)
         this.animeItems()
-
     }
-
     animeItems = () => {
         this.items.forEach((value, index) => {
             value.style.animation ? (value.style.animation = "") :
                 (value.style.animation = `showitems ${index / 0.5}s ease forwards 0.3`)
         })
-
     }
-
-
     clickOnIcon = () => {
         this.mobileicon.addEventListener('click', this.bringNavBar)
     }
-
     render = () => {
         if (this.mobileicon) {
             this.clickOnIcon()
         }
     }
 }
-
 const show = new aside('.mobileIcon', '.nav-bar', '.items')
 show.render()
