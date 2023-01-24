0c5f285a49b625756d590005d07892e7225fa666
Erick Yuta <erick_miyagi34231@hotmail.com>
2023-01-24 16:27:55 -0300

    Carrousel

--- js/code.js
+++ js/code.js
@@ -1,43 +1,15 @@
-const slider = document.querySelectorAll('.slider');
-const btnPrev = document.getElementById('prev-button');
-const btnNext = document.getElementById('next-button');
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
 
-let currentSlide = 0;
-
-function hideSlider() {
-    slider.forEach(item => item.classList.remove('on'))
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
 }
 
-function showSlider() {
-    slider[currentSlide].classList.add('on')
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
 }
-
-function nextSlider() {
-    hideSlider()
-    if (currentSlide === slider.length - 1) {
-        currentSlide = 0
-    } else {
-        currentSlide++
-    }
-    showSlider()
-}
-
-function prevSlider() {
-    hideSlider()
-    if (currentSlide === 0) {
-        currentSlide = slider.length - 1
-    } else {
-        currentSlide--
-    }
-    showSlider()
-}
-
-btnNext.addEventListener('click', nextSlider)
-btnPrev.addEventListener('click', prevSlider)
-
-
-console.log(slider)
-
-
-currentSlide = currentSlide + 1
-currentSlide = currentSlide - 1
+setInterval(next, 5000);
